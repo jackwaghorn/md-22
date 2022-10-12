375b0f39abecdca466933c23843eff56ca40fec2
Jack Waghorn <jackwaghorn@Jacks-MacBook-Pro.local>
2022-10-12 19:25:30 +0100

    again

--- netlify/functions/prismic.js
+++ netlify/functions/prismic.js
@@ -3,25 +3,26 @@ const Prismic = require('@prismicio/client')
 exports.handler = async function () {
   const client = Prismic.client('http://mollyelizabeth.cdn.prismic.io/api')
   return client
-  .query(Prismic.Predicates.at("document.type", "shop")) // An empty query will return all the documents
-  .then(function (response) {
-    console.log(response);
-    let newItems = Object.values(response.results[0].data.body).map(item => {
+    .query(Prismic.Predicates.at('document.type', 'shop')) // An empty query will return all the documents
+    .then(function (response) {
+      console.log(response)
+      let newItems = Object.values(response.results[0].data.body).map(item => {
+        return {
+          id: item
+          // .id,
+          // name: item.primary.item_title,
+          // price: item.primary.price,
+          // url: 'https://md-shop-test.netlify.app/.netlify/functions/prismic'
+        }
+      })
+
       return {
-        id: item.id,
-        name: item.primary.item_title,
-        price: item.primary.price,
-        url: 'https://md-shop-test.netlify.app/.netlify/functions/prismic'
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify(newItems)
       }
     })
-
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json'
-      },
-      body: JSON.stringify(newItems)
-    }
-  })
 }
