b232fde33fb1b4531eb04e7383fd0be5f17ef1cc
Jack Waghorn <jackwaghorn@Jacks-MacBook-Pro.local>
2022-10-12 19:48:32 +0100

    again

--- netlify/functions/prismic.js
+++ netlify/functions/prismic.js
@@ -3,15 +3,14 @@ const Prismic = require('@prismicio/client')
 exports.handler = async function () {
   const client = Prismic.client('http://mollyelizabeth.cdn.prismic.io/api')
   return client.getSingle('shop').then(function (response) {
-    // console.log(response)
-    // let newItems = Object.values(response.results[0].data.body).map(item => {
-    //   return {
-    //     id: item.id,
-    //     name: item.primary.item_title,
-    //     price: item.primary.price,
-    //     url: 'https://md-shop-test.netlify.app/.netlify/functions/prismic'
-    //   }
-    // })
+    let newItems = Object.values(response.data.body).map(item => {
+      return {
+        id: item.id,
+        name: item.primary.item_title,
+        price: item.primary.price,
+        url: 'https://md-shop-test.netlify.app/.netlify/functions/prismic'
+      }
+    })
 
     return {
       statusCode: 200,
@@ -19,7 +18,7 @@ exports.handler = async function () {
         'Content-Type': 'application/json',
         Accept: 'application/json'
       },
-      body: JSON.stringify({id:'update'})
+      body: JSON.stringify(newItems)
     }
   })
 }
