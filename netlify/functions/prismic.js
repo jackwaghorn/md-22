5bc01dbdafe0b7a5214622bb641c427b3e8cec04
Jack Waghorn <jackwaghorn@Jacks-MacBook-Pro.local>
2022-10-13 12:22:19 +0100

    t

--- netlify/functions/prismic.js
+++ netlify/functions/prismic.js
@@ -15,11 +15,10 @@ exports.handler = async function () {
      
           Object.values(response.results[0].data.shop.body).map(item => {
             return {
-              id: item
-              // .id,
-              // name: item.primary.item_title,
-              // price: item.primary.price,
-              // url: 'https://md-shop-test.netlify.app/.netlify/functions/prismic'
+              id: item.id,
+              name: item.item_title,
+              price: item.price,
+              url: 'https://md-shop-test.netlify.app/.netlify/functions/prismic'
             }
           })
         )
