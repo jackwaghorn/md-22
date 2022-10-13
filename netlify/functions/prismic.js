79b1b2c1fad28ef405ee37e8403cab4c355c5d5d
Jack Waghorn <jackwaghorn@Jacks-MacBook-Pro.local>
2022-10-13 12:08:44 +0100

    upte

--- netlify/functions/prismic.js
+++ netlify/functions/prismic.js
@@ -12,7 +12,7 @@ exports.handler = async function () {
           Accept: 'application/json'
         },
         body: JSON.stringify(
-          response.results[0].data.body
+          response.results[0].data
           // Object.values(response.results[0].data.body).map(item => {
           //   return {
           //     id: item
