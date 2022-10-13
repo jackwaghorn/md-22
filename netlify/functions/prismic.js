fded9fd90204e09636fb78680f397f88ec526edc
Jack Waghorn <jackwaghorn@Jacks-MacBook-Pro.local>
2022-10-13 14:07:44 +0100

    upre

--- netlify/functions/prismic.js
+++ netlify/functions/prismic.js
@@ -26,28 +26,3 @@ exports.handler = async function () {
       }
     })
 }
-
-// const Prismic = require('@prismicio/client')
-
-// exports.handler = async function () {
-//   const client = Prismic.client('http://mollyelizabeth.cdn.prismic.io/api')
-//   return client.getSingle('shop').then(function (response) {
-//     let newItems = Object.values(response.data.body).map(item => {
-//       return {
-//         id: item.id,
-//         name: item.primary.item_title,
-//         price: item.primary.price,
-//         url: 'https://md-shop-test.netlify.app/.netlify/functions/prismic'
-//       }
-//     })
-
-//     return {
-//       statusCode: 200,
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Accept: 'application/json'
-//       },
-//       body: JSON.stringify(newItems)
-//     }
-//   })
-// }
