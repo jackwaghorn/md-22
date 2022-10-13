0ab99fb4cb7dcad339a27ff522a7bb5bfcb2d5aa
Jack Waghorn <jackwaghorn@Jacks-MacBook-Pro.local>
2022-10-13 12:23:26 +0100

    upte

--- netlify/functions/prismic.js
+++ netlify/functions/prismic.js
@@ -12,12 +12,12 @@ exports.handler = async function () {
           Accept: 'application/json'
         },
         body: JSON.stringify(
-     
           Object.values(response.results[0].data.shop.body).map(item => {
             return {
-              id: item.id,
-              name: item.item_title,
-              price: item.price,
+              id: item,
+              // .id,
+              // name: item.item_title,
+              // price: item.price,
               url: 'https://md-shop-test.netlify.app/.netlify/functions/prismic'
             }
           })
