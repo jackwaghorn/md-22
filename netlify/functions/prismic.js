f10a49987df9d7a214932621a36ba8da9edbc0c3
Jack Waghorn <jackwaghorn@Jacks-MacBook-Pro.local>
2022-11-30 18:03:13 +0000

    shop

--- netlify/functions/prismic.js
+++ netlify/functions/prismic.js
@@ -3,24 +3,29 @@ const Prismic = require('@prismicio/client')
 exports.handler = async function () {
   const client = Prismic.client('http://mollyelizabeth.cdn.prismic.io/api')
   return client
-    .query(Prismic.Predicates.at('document.type', 'shop')) // An empty query will return all the documents
-    .then(response => {
+    .query(Prismic.Predicates.at('document.type', 'shop_item')) // An empty query will return all the documents
+    .then(function (response) {
+      console.log('Documents: ', response.results)
+
+      let data = response.results
+      console.log(response.results)
+
+      let newItems = Object.values(data).map(item => {
+        return {
+          // name: item.data.product.name.value[0].text,
+          // id: item.id,
+          price: item
+          // url: 'https://mollydooner.com/.netlify/functions/prismic',
+        }
+      })
+
       return {
         statusCode: 200,
         headers: {
           'Content-Type': 'application/json',
           Accept: 'application/json'
         },
-        body: JSON.stringify(
-          Object.values(response.results[0].data.shop.body.value).map(item => {
-            return {
-              id: item.id,
-              title: item['non-repeat'].item_title.value,
-              price: item['non-repeat'].price.value,
-              url: 'https://mollydooner.com/.netlify/functions/prismic'
-            }
-          })
-        )
+        body: JSON.stringify(newItems)
       }
     })
 }
